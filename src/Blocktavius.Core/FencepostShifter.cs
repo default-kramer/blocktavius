3496695579185706d9897d06cb70c04bc29219b6
Ryan Kramer <default.kramer@gmail.com>
2025-09-28 17:41:50 -0500

    doing it myself

--- src/Blocktavius.Core/FencepostShifter.cs
+++ src/Blocktavius.Core/FencepostShifter.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -46,29 +47,23 @@ It is only the Resolve step which is tricky, but I believe the following approac
 First realize that only fences can become invalid, by violating either
 the MinFenceLength or MaxFenceLength constraints.
 So let's assume we are trying to resolve the fence bounded by orig[4] and orig[5].
-If this fence is too long, we have to pull the left and/or right neighbors closer.
-If this fence is too shot, we have to push the left and/or right neighbors away.
+If this fence is too long, we have to pull the left and/or right posts closer.
+If this fence is too shot, we have to push the left and/or right posts away.
 There are 4 operations, all of which produce a "ResolutionPlan":
   * pull left closer
   * pull right closer
   * push left away
   * push right away
-Each of these operations is given a fenceIndex and the requested amount of space.
-Here is the pseudocode for the PushLeftAway() operation
-  * leftPostIndex = fenceIndex - 1
-  * rightPostIndex = fenceIndex
-  * Compute how much space we can provide by adjusting the right post only.
-    If this amount is exceeds the requested space, we are done.
-  * Else we have to consider moving the left post too. We start by computing
-  * maxPossibleLeftPostMove = leftPost.CurrentValue - leftPost.IroncladRange.Start
-  * if maxPossibleLeftPostMove == 0 we can immediately return
-  * actualLeftPostMove = recursive PushLeftAway(fenceIndex - 1, spaceRequested: maxPossiblePostMove)
-  * now we have the final amount of space that can be freed and we are done
-I think the ResolutionPlan that is returned should have something like this:
-  * int AvailableSpace
-  * Stack<(int postIndex, int plannedIndex)> PlannedAdjustments
+All 4 of these operations are recursive, and accept { int postIndex; int requestedSpace }.
+Here pseudocode for PushLeft(i, requestedSpace)
+  define easySpace = how far left posts[i] can move without moving posts[i-1]
+  if easySpace >= requestedSpace
+    return requestedSpace
+  else
+    define hardSpace = recurse PushLeft(i-1, requestedSpace - easySpace)
+    return easySpace + hardSpace
+You can imagine the other 3 operations would be similar.
 
-The other 3 operations would work in a similar recursive manner to PushLeftAway().
 
 Here example pseudocode of a complete resolution.
   int thisFenceLength = shifted[5]-shifted[4]
@@ -100,13 +95,12 @@ Overall progress
 [ ] Convince me that you understand the proposed algorithm and agree with its soundness.
     (It would be even better if you can come up with a better algorithm.)
     Also let me know if you agree with this checklist or want to change it.
-[ ] Remove any useless leftover hacking from inside this class.
 [ ] Create and test the ResolutionPlan mechanism.
     Tests should prove that even if the "random nudge" phase is totally broken
     (imagine that it assigns totally random values instead of a value from the
      ironclad range), the ResolutionPlan mechanism *still* works!
     (I think this is true; if not we can relax it.)
-[ ] Complete the algorithm. In a new file, add statistical tests similar to those of ExerciseCornerShifter.
+[ ] Complete the algorithm and add statistical tests similar to those of ExerciseCornerShifter.
 */
 internal class FencepostShifter
 {
@@ -118,72 +112,129 @@ internal class FencepostShifter
 		public required int MaxFenceLength { get; init; }
 	}
 
-	record struct Post(int X, Range ironcladRange);
+	/// <summary>
+	/// An index that would be out of bounds returns the Left/Right Anchor value instead.
+	/// Simplifies code a bit by allowing you to unconditionally get a neighbor
+	/// without checking that i-1 or i+1 is in range.
+	/// </summary>
+	sealed class Postlist<T> : IReadOnlyList<T>
+	{
+		private readonly IReadOnlyList<T> items;
+		public Postlist(IEnumerable<T> items)
+		{
+			this.items = items.ToList();
+		}
 
-	// AHA!!
-	// Perhaps the recursive question is "How much space can you free up by only moving posts to the left?"
-	//private readonly IReadOnlyList<Post> orig = null!;
-	//private readonly List<Post> shifted = null!;
+		public required T LeftAnchor { get; init; }
+		public required T RightAnchor { get; init; }
 
-	//private bool CanPushToLeft(int i, int spaceNeeded, ref int postMovesNeeded, Settings settings)
-	//{
-	//	// WARNING - I think this is not quite accurate.
-	//	// What is `i` here, a post index or fence index or both??
+		public T this[int index]
+		{
+			get
+			{
+				if (index < 0) { return LeftAnchor; }
+				if (index < items.Count) { return items[index]; }
+				return RightAnchor;
+			}
+		}
 
-	//	// YES I THINK THIS IS THE KEY!
-	//	// To be extra certain
-	//	// 1) Load initial problem
-	//	// 2) Ensure that it is valid per settings
-	//	// 3) Randomly nudge all posts
-	//	// 4) While anything is invalid, pick one invalid post at random and resolve it
-	//	//
-	//	// NOTE: This method should probably return an int instead of a bool.
-	//	// And the caller will check whether there was enough.
-	//	// SOmething like this:
-	//	// var spaceToLeft = FreeSpaceLeft(i-1, spaceNeeded)
-	//	// var spaceToRight = FreeSpaceRight(i+1, spaceNeeded)
-	//	// if(spaceToLeft + spaceToRight < spaceNeeded) ASSERT FAIL "no solution??"
-	//	// else decide how to divvy it up amongst left and right
-	//	//
-	//	// WHY is this seeming more clear than previous implementations?
-	//	// I think the novelty here is separating the 4 questions:
-	//	// * how much space can be cleared by pushing to the left?
-	//	// * same for right?
-	//	// * how much space can be absorbed by pulling from the left?
-	//	// * same for right?
-	//	//
-	//	// With these in hand, I think we can head DIRECTLY to a solution
-	//	// instead of spinning the PRNG until something comes up valid.
+		public int Count => items.Count;
 
-	//	if (spaceNeeded < 1)
-	//	{
-	//		return true;
-	//	}
+		public IEnumerator<T> GetEnumerator() => items.GetEnumerator();
 
-	//	var myPost = shifted[i];
-	//	int myRunLength = shifted[i + 1].X - myPost.X;
+		IEnumerator IEnumerable.GetEnumerator() => items.GetEnumerator();
+	}
 
-	//	int possibleRunShrinkage = myRunLength - settings.MinFenceLength;
-	//	bool canSimplyShortenRun = possibleRunShrinkage >= spaceNeeded;
+	sealed class Post
+	{
+		public required int X { get; init; }
+		public required Range IroncladRange { get; init; }
+	}
 
-	//	if (canSimplyShortenRun || i == 0)
-	//	{
-	//		return canSimplyShortenRun;
-	//	}
+	sealed class MutablePost
+	{
+		public required int X { get; set; }
+		public required Range IroncladRange { get; init; }
 
-	//	int postShiftNeeded = spaceNeeded - possibleRunShrinkage;
-	//	int maxPossiblePostShift = myPost.X - myPost.ironcladRange.xMin;
-	//	if (postShiftNeeded > maxPossiblePostShift)
-	//	{
-	//		return false;
-	//	}
-	//	if (!CanPushToLeft(i - 1, postShiftNeeded, ref postShiftNeeded, settings))
-	//	{
-	//		return false;
-	//	}
-	//	postMovesNeeded++;
-	//	return true;
-	//}
+		public static MutablePost Copy(Post post) => new MutablePost { X = post.X, IroncladRange = post.IroncladRange };
+	}
+
+	private readonly Postlist<Post> orig;
+	private readonly Postlist<MutablePost> shifted;
+	private readonly Settings settings;
+
+	public FencepostShifter(IReadOnlyList<int> orig, Settings settings)
+	{
+		this.settings = settings;
+
+		var ranges = BuildIroncladRanges(orig, settings);
+
+		var posts = orig.Zip(ranges).Select(x => new Post() { X = x.First, IroncladRange = x.Second }).ToList();
+		this.orig = new Postlist<Post>(posts)
+		{
+			LeftAnchor = new Post() { X = 0, IroncladRange = new Range(0, 0) },
+			RightAnchor = new Post() { X = settings.TotalLength, IroncladRange = new Range(settings.TotalLength, settings.TotalLength) }
+		};
+
+		this.shifted = new Postlist<MutablePost>(this.orig.Select(MutablePost.Copy).ToList())
+		{
+			LeftAnchor = MutablePost.Copy(this.orig.LeftAnchor),
+			RightAnchor = MutablePost.Copy(this.orig.RightAnchor),
+		};
+	}
+
+	sealed class ResolutionPlan
+	{
+		public Stack<(int postIndex, int newPosition)> PlannedMoves { get; private init; } = new();
+		public required int RequestedSpace { get; init; }
+		public int AvailableSpace { get; set; } = 0;
+		public int NeededSpace => RequestedSpace - AvailableSpace;
+		public bool IsDone => AvailableSpace >= RequestedSpace;
+
+		public ResolutionPlan CreateRecursivePlan(int requestSpace) => new ResolutionPlan()
+		{
+			AvailableSpace = 0,
+			RequestedSpace = requestSpace,
+			PlannedMoves = this.PlannedMoves, // use the same mutable stack!
+		};
+	}
+
+	private void PushPostsLeft(int postIndex, ResolutionPlan plan)
+	{
+		if (postIndex < 0 || plan.IsDone)
+		{
+			return;
+		}
+
+		var post = shifted[postIndex];
+		int leftPostMaxPossibleShift = post.X - post.IroncladRange.xMin;
+		int requestedSpace = Math.Min(leftPostMaxPossibleShift, plan.NeededSpace);
+		if (requestedSpace < 1)
+		{
+			return;
+		}
+
+		// Compute "easy space" as the amount of space we can free up without recursing.
+		int easyShiftPosition = shifted[postIndex - 1].X + settings.MinFenceLength;
+		int easySpace = post.X - easyShiftPosition;
+		if (easySpace >= requestedSpace)
+		{
+			plan.AvailableSpace += requestedSpace;
+			plan.PlannedMoves.Push((postIndex, post.X - requestedSpace));
+			return;
+		}
+
+		// The "easy space" is not enough, we have to recurse
+		var recurse = plan.CreateRecursivePlan(requestedSpace - easySpace);
+		PushPostsLeft(postIndex - 1, recurse);
+		int hardSpace = Math.Min(recurse.AvailableSpace, recurse.RequestedSpace);
+
+		// Either we've satisfied the request, or we've gone all the way to the left
+		// and there's nothing more we can do.
+		int totalSpace = easySpace + hardSpace;
+		plan.AvailableSpace += totalSpace;
+		plan.PlannedMoves.Push((postIndex, post.X - totalSpace));
+	}
 
 	private static IReadOnlyList<Range> BuildIroncladRanges(IReadOnlyList<int> posts, Settings settings)
 	{
@@ -208,12 +259,80 @@ internal class FencepostShifter
 				range = range.ConstrainRight(posts[i + 1] - settings.MinFenceLength);
 			}
 
-
-			// TODO is the following correct?
-			range = range.ConstrainLeft((i - 1) * settings.MinFenceLength);
-			range = range.ConstrainRight(i * settings.MaxFenceLength);
+			ranges[i] = range;
 		}
 
 		return ranges;
+	}
+
+	internal class TestApi
+	{
+		private Settings settings;
+		private IReadOnlyList<int> orig;
+		private FencepostShifter shifter;
+
+		public TestApi()
+		{
+			settings = new Settings()
+			{
+				MinFenceLength = 1,
+				MaxFenceLength = 99,
+				TotalLength = 99,
+				MaxNudge = 99,
+			};
+
+			orig = [7, 20, 30];
+
+			shifter = new(orig, settings);
+		}
+
+		public TestApi WithMinFenceLength(int minFenceLength)
+		{
+			settings = settings with { MinFenceLength = minFenceLength };
+			return this;
+		}
+
+		public TestApi WithMaxFenceLength(int maxFenceLength)
+		{
+			settings = settings with { MaxFenceLength = maxFenceLength };
+			return this;
+		}
+
+		public TestApi WithMaxNudge(int maxNudge)
+		{
+			settings = settings with { MaxNudge = maxNudge };
+			return this;
+		}
+
+		public TestApi Reload(params int[] posts)
+		{
+			this.orig = posts;
+			this.shifter = new FencepostShifter(orig, settings);
+			return this;
+		}
+
+		public string Print(string format = "D2")
+		{
+			var sb = new StringBuilder();
+			sb.Append($"[0] ");
+			sb.Append(string.Join(" ", shifter.shifted.Select(post => post.X.ToString(format))));
+			sb.Append($" [{shifter.settings.TotalLength}]");
+			return sb.ToString();
+		}
+
+		public int PushLeft(string postName, int amount)
+		{
+			int postValue = int.Parse(postName);
+			var postIndex = shifter.shifted.Index().Where(p => p.Item.X == postValue).Single().Index;
+
+			var plan = new ResolutionPlan() { RequestedSpace = amount };
+			shifter.PushPostsLeft(postIndex, plan);
+			foreach (var move in plan.PlannedMoves)
+			{
+				shifter.shifted[move.postIndex].X = move.newPosition;
+			}
+
+			return plan.AvailableSpace;
+		}
 	}
 }
---
+++ src/Blocktavius.Tests/FencepostShifterTests.cs
@@ -0,0 +1,87 @@
+﻿using Blocktavius.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static Blocktavius.Core.FencepostShifter;
+
+namespace Blocktavius.Tests;
+
+[TestClass]
+public class FencepostShifterTests
+{
+	private static TestApi NewShifter() => new TestApi();
+
+	[TestMethod]
+	public void push_left_vs_min_fence_length()
+	{
+		var shifter = NewShifter()
+			.WithMinFenceLength(10)
+			.Reload(10, 30);
+
+		Assert.AreEqual("[0] 10 30 [99]", shifter.Print());
+		Assert.AreEqual(6, shifter.PushLeft("30", 6));
+		Assert.AreEqual("[0] 10 24 [99]", shifter.Print());
+		Assert.AreEqual(4, shifter.PushLeft("24", 6));
+		Assert.AreEqual("[0] 10 20 [99]", shifter.Print());
+		Assert.AreEqual(0, shifter.PushLeft("20", 1));
+		Assert.AreEqual(0, shifter.PushLeft("10", 1));
+		Assert.AreEqual("[0] 10 20 [99]", shifter.Print());
+	}
+
+	[TestMethod]
+	public void push_left_vs_max_nudge()
+	{
+		var shifter = NewShifter()
+			.WithMaxNudge(10)
+			.Reload(40, 70);
+
+		Assert.AreEqual("[0] 40 70 [99]", shifter.Print());
+		Assert.AreEqual(7, shifter.PushLeft("40", 7));
+		Assert.AreEqual("[0] 33 70 [99]", shifter.Print());
+		Assert.AreEqual(3, shifter.PushLeft("33", 7));
+		Assert.AreEqual("[0] 30 70 [99]", shifter.Print());
+	}
+
+	[TestMethod]
+	public void push_left_against_invalid_state()
+	{
+		var shifter = NewShifter()
+			.WithMaxNudge(3)
+			.WithMinFenceLength(10)
+			.Reload(11, 22, 45, 48);
+
+		// Initial state is not valid, 45---48 violates MinFenceLength=10
+		Assert.AreEqual("[0] 11 22 45 48 [99]", shifter.Print());
+
+		// We try to push 48 left...
+		Assert.AreEqual(-4, shifter.PushLeft("48", 1));
+		Assert.AreEqual("[0] 11 22 42 52 [99]", shifter.Print());
+		// ... It does its best and succeeds at pushing 45 to 42 (max nudge = 3)
+		// and returns the available space of -4.
+		// The fact that the test API immediately applies that -4, moving 48 to 52, is
+		// a somewhat arbitrary decision affecting tests only.
+		// Real scenarios would call both PushLeft and PushRight and look at both
+		// plans before deciding what to do.
+		// (Probably prefer to move as few posts as possible.)
+	}
+
+	[TestMethod]
+	public void push_left_recursion()
+	{
+		var shifter = NewShifter()
+			.WithMinFenceLength(10)
+			.Reload(11, 22, 33, 44, 51);
+
+		Assert.AreEqual("[0] 11 22 33 44 51 [99]", shifter.Print());
+		Assert.AreEqual(1, shifter.PushLeft("51", 99));
+		Assert.AreEqual("[0] 10 20 30 40 50 [99]", shifter.Print());
+
+		// reload same array, but push 44 this time, leaving 51 in place
+		shifter.Reload(11, 22, 33, 44, 51);
+		Assert.AreEqual("[0] 11 22 33 44 51 [99]", shifter.Print());
+		Assert.AreEqual(4, shifter.PushLeft("44", 99));
+		Assert.AreEqual("[0] 10 20 30 40 51 [99]", shifter.Print());
+	}
+}
