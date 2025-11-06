e651e8a019b9d206e4e19ce72be53cc63517fb7b
Ryan Kramer <default.kramer@gmail.com>
2025-11-06 12:23:27 -0600

    minor cleanup

--- src/Blocktavius.AppDQB2/App.xaml.cs
+++ src/Blocktavius.AppDQB2/App.xaml.cs
@@ -43,7 +43,6 @@ namespace Blocktavius.AppDQB2
 			}
 
 			var vm = new ProjectVM(selectedProfile);
-			//vm.StgdatFilePath = @"C:\Users\kramer\Documents\My Games\DRAGON QUEST BUILDERS II\Steam\76561198073553084\SD\B02\STGDAT01.BIN";
 			vm.ProjectFilePath = @"C:\Users\kramer\Documents\code\HermitsHeresy\examples\STB\foo.blocktaviusproject";
 
 			vm.Scripts.Add(new ScriptVM() { Name = "Main" });
