d4dba34bb162c5ad02ceca7593397943ef06b720
Ryan Kramer <default.kramer@gmail.com>
2025-11-22 09:42:43 -0600

    new stage loader

---
+++ src/Blocktavius.AppDQB2/Services/IServices.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blocktavius.AppDQB2.Services;
+
+interface IServices
+{
+	IStageLoader StageLoader();
+}
+
+sealed class DefaultServices : IServices
+{
+	private readonly IStageLoader stageLoader = new StageLoader();
+
+	private DefaultServices() { }
+
+	public static readonly DefaultServices Instance = new();
+
+	public IStageLoader StageLoader() => stageLoader;
+}
