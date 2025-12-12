8a3ea623709c734c9208c3534c5447516aa1f028
Ryan Kramer <default.kramer@gmail.com>
2025-12-12 11:56:36 -0600

    cleanup

--- src/Antipasta/IPropagationContext.cs
+++ src/Antipasta/IPropagationContext.cs
@@ -6,24 +6,32 @@ using System.Threading.Tasks;
 
 namespace Antipasta;
 
-/// <remarks>
-/// NOTE - This interface is readonly by design.
-/// The plan is that nodes should report their <see cref="PropagationResult"/>
-/// and then the machinery will use <see cref="GraphManager.GetListeners"/> to merge
-/// all those listeners into the propagation queue.
-/// </remarks>
 public interface IPropagationContext
 {
+	/// <summary>
+	/// This is internalized because only framework-ish code should access it.
+	/// Application code should inherit from some base class that ensures the scheduler is used
+	/// correctly (such as the <see cref="AsyncDerivedElement{TComputer, TInput, TOutput}"/>).
+	/// </summary>
 	Internalized<Scheduling.IAsyncScheduler> AsyncScheduler { get; }
 
+	/// <summary>
+	/// Phase 1 is Setup.
+	/// This is when all requested changes (<see cref="IChangeset"/>) are being applied.
+	/// </summary>
 	Progress SetupProgress { get; }
 
+	/// <summary>
+	/// Phase 2 is Propagation.
+	/// This is when change notifications travel to listeners.
+	/// </summary>
 	Progress PropagationProgress { get; }
 
 	/// <summary>
-	/// True while the <see cref="INodeGroup.OnChanged(IImmediateNotifyNode)"/> callback is being executed.
+	/// True while the <see cref="INodeGroup.NotifyPropertyChanged"/> callback is being executed.
 	/// Can be used to detect, for example, the situation where you raise a notification that an ItemsSource
 	/// property has changed and WPF attempts to set a SelectedItem property to null.
+	/// (This can happen in both the Setup and the Propagation phases.)
 	/// </summary>
 	bool IsNotifying { get; }
 }
