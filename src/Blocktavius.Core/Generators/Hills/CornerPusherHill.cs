eb77d3fd3a9ba490d38a7645274b75964636f2e1
Ryan Kramer <default.kramer@gmail.com>
2026-02-04 21:55:10 -0600

    clean up CornerPusherHill

--- src/Blocktavius.Core/Generators/Hills/CornerPusherHill.cs
+++ src/Blocktavius.Core/Generators/Hills/CornerPusherHill.cs
@@ -1,9 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Collections.Immutable;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Blocktavius.Core.Generators.Hills;
 
@@ -38,6 +35,9 @@ public static class CornerPusherHill
 		public decimal MaxInitialMissPercent { get; init; } = 1.0m;
 	}
 
+	/// <summary>
+	/// Builds a hill using integer elevations.
+	/// </summary>
 	public static I2DSampler<int> BuildHill(Settings settings, Shell shell)
 	{
 		if (shell.IsHole)
@@ -46,232 +46,211 @@ public static class CornerPusherHill
 		}
 
 		var area = new ExpandableArea<int>(shell);
-		var algo = new IntAlgorithm();
-		algo.BuildHill(settings, area);
+		BuildHill(settings, area, i => i);
 
 		return area.GetSampler(ExpansionId.MaxValue, settings.MaxElevation)
 			.Project(tuple => tuple.Item1 ? tuple.Item2 : -1);
 	}
 
+	/// <summary>
+	/// Generic implementation for building a hill, converting integer elevations to type <typeparamref name="T"/>.
+	/// </summary>
 	internal static void BuildHill<T>(Settings settings, ExpandableArea<T> area, Func<int, T> converter)
 	{
-		var algo = new GenericAlgorithm<T>() { Converter = converter };
-		algo.BuildHill(settings, area);
+		// This dictionary tracks items on the shell that are candidates for being "pushed" out.
+		var pendingExpansion = new Dictionary<XZ, Layer.PendingShellItem>();
+
+		// Initialize the first layer and its miss counts.
+		InitializeFirstLayer(settings, area.CurrentShell(), pendingExpansion);
+
+		// Iteratively build each layer from the top down.
+		int numLayers = settings.MaxElevation - settings.MinElevation;
+		var ring = new ShellItemRing(area.CurrentShell());
+		for (int i = 0; i < numLayers; i++)
+		{
+			int currentElevation = settings.MaxElevation - i;
+			var layer = new Layer(ring, pendingExpansion, settings);
+
+			// Calculate and apply the expansion for the current layer.
+			var expansion = layer.CalculateExpansion();
+			if (expansion.Count > 0)
+			{
+				area.Expand(expansion.Select(e => (e.XZ, converter(e.Elevation))));
+				foreach (var (xz, _) in expansion)
+				{
+					pendingExpansion.Remove(xz);
+				}
+			}
+
+			// Update miss counts for the items that will carry over to the next layer.
+			ring = new ShellItemRing(area.CurrentShell());
+			UpdateMisses(pendingExpansion, ring, currentElevation - 1);
+		}
+
+		// Twist ending? The "pending" expansion is actually expansion we already committed to.
+		// This was mostly so that the miss count dictionary and the shell items line up nicely,
+		// but may also be a consequence of preserving legacy behavior.
+		var finalExpansion = pendingExpansion.Select(kvp => (kvp.Key, converter(kvp.Value.Elevation)));
+		area.Expand(finalExpansion);
 	}
 
-	abstract class Algorithm<T>
+	private static void InitializeFirstLayer(Settings settings, IReadOnlyList<ShellItem> initialShellItems, Dictionary<XZ, Layer.PendingShellItem> pendingExpansion)
 	{
-		public void BuildHill(Settings settings, ExpandableArea<T> area)
+		UpdateMisses(pendingExpansion, new ShellItemRing(initialShellItems), settings.MaxElevation);
+
+		// Overwrite legacy initial miss counts if percentage range is valid.
+		decimal minPercent = Math.Clamp(settings.MinInitialMissPercent, 0m, 1m);
+		decimal maxPercent = Math.Clamp(settings.MaxInitialMissPercent, 0m, 1m);
+		int minMisses = Convert.ToInt32(minPercent * settings.MaxConsecutiveMisses);
+		int maxMisses = Convert.ToInt32(maxPercent * settings.MaxConsecutiveMisses);
+
+		bool reseed = (minMisses > 0 || maxMisses > 0)
+			&& maxMisses >= minMisses
+			&& settings.MinInitialMissPercent >= 0
+			&& settings.MaxInitialMissPercent >= 0;
+
+		if (reseed)
 		{
-			var layer = Layer.FirstLayer(area, settings, this);
-			BuildHill(settings, layer);
-		}
-
-		protected abstract IEnumerable<(XZ, T)> ConvertExpansion(IEnumerable<(XZ, int)> expansion);
-
-		private void BuildHill(Settings settings, Layer firstLayer)
-		{
-			var layers = new Stack<Layer>();
-			layers.Push(firstLayer);
-
-			int needLayers = settings.MaxElevation - settings.MinElevation + 1;
-			while (layers.Count < needLayers)
+			var prng = settings.Prng.Clone();
+			foreach (var kvp in pendingExpansion)
 			{
-				layers.Push(layers.Peek().NextLayer(settings.Prng));
+				kvp.Value.MissCount = prng.NextInt32(minMisses, maxMisses + 1);
 			}
-
-			var finalLayer = layers.First();
-			var area = finalLayer.area;
-			var finalExpansion = finalLayer.pendingExpansion.Select(kvp => (kvp.Key, kvp.Value.Elevation)).ToList();
-			area.Expand(ConvertExpansion(finalExpansion));
 		}
+	}
 
-		sealed class Layer
+	private static void UpdateMisses(Dictionary<XZ, Layer.PendingShellItem> pendingItems, IEnumerable<ShellItem> shellItems, int elevation)
+	{
+		// reduce inside corners weight from 3:1 down to 2:1
+		foreach (var item in shellItems.Where(i => i.CornerType != CornerType.Inside))
 		{
-			public sealed class PendingShellItem
+			if (pendingItems.TryGetValue(item.XZ, out var pendingItem))
 			{
-				public int MissCount { get; set; } // mutable
-
-				/// <summary>
-				/// We retain the elevation this item was *introduced* at to maintain
-				/// compatability with a previous version of this algorithm.
-				/// (Also because I can't figure out why it gets more jagged when we break compatability.)
-				/// </summary>
-				public int Elevation { get; init; }
+				pendingItem.MissCount++;
 			}
+			else
+			{
+				pendingItems[item.XZ] = new Layer.PendingShellItem { MissCount = 1, Elevation = elevation };
+			}
+		}
+	}
+
+	/// <summary>
+	/// Represents the logic for calculating the expansion of a single elevation layer.
+	/// </summary>
+	private sealed class Layer
+	{
+		public sealed class PendingShellItem
+		{
+			public int MissCount { get; set; } // mutable
 
 			/// <summary>
-			/// A "miss" occurs whenever we decide not to push a shell item out for the next layer.
-			/// This dictionary tracks consecutive miss counts; it resets to 0 when we push that XZ.
-			/// Permitting a larger number of consecutive misses creates a more vertical (steeper) hill.
+			/// We retain the elevation this item was *introduced* at to maintain
+			/// compatability with a previous version of this algorithm.
+			/// (Also because I can't figure out why it gets more jagged when we break compatability.)
 			/// </summary>
-			public readonly Dictionary<XZ, PendingShellItem> pendingExpansion;
-
-			public readonly Algorithm<T> algorithm;
-			public readonly ExpandableArea<T> area;
-			public readonly ShellItemRing shellItems;
-			public readonly int elevation;
-			public readonly Settings settings;
-
-			private static void UpdateMisses(Dictionary<XZ, PendingShellItem> pendingItems, IEnumerable<ShellItem> shellItems, int elevation)
-			{
-				// reduce inside corners weight from 3:1 down to 2:1
-				foreach (var item in shellItems.Where(i => i.CornerType != CornerType.Inside))
-				{
-					if (pendingItems.TryGetValue(item.XZ, out var pendingItem))
-					{
-						pendingItem.MissCount++;
-					}
-					else
-					{
-						pendingItems[item.XZ] = new PendingShellItem { MissCount = 1, Elevation = elevation };
-					}
-				}
-			}
-
-			private Layer(Layer prev)
-			{
-				this.algorithm = prev.algorithm;
-				this.area = prev.area;
-				this.pendingExpansion = prev.pendingExpansion;
-				this.shellItems = new ShellItemRing(area.CurrentShell());
-				this.elevation = prev.elevation - 1;
-				this.settings = prev.settings;
-
-				UpdateMisses(pendingExpansion, shellItems, elevation);
-			}
-
-			private Layer(ExpandableArea<T> area, Settings settings, Algorithm<T> algorithm)
-			{
-				this.algorithm = algorithm;
-				this.area = area;
-				this.shellItems = new ShellItemRing(area.CurrentShell());
-				this.pendingExpansion = new();
-				this.elevation = settings.MaxElevation;
-				this.settings = settings;
-
-				UpdateMisses(pendingExpansion, shellItems, elevation);
-			}
-
-			public static Layer FirstLayer(ExpandableArea<T> area, Settings settings, Algorithm<T> algorithm)
-			{
-				var layer = new Layer(area, settings, algorithm);
-
-				// Overwrite legacy initial miss counts if percentage range is valid.
-				decimal minPercent = Math.Clamp(settings.MinInitialMissPercent, 0m, 1m);
-				decimal maxPercent = Math.Clamp(settings.MaxInitialMissPercent, 0m, 1m);
-				int minMisses = Convert.ToInt32(minPercent * settings.MaxConsecutiveMisses);
-				int maxMisses = Convert.ToInt32(maxPercent * settings.MaxConsecutiveMisses);
-				bool reseed = (minMisses > 0 || maxMisses > 0)
-					&& maxMisses >= minMisses
-					&& settings.MinInitialMissPercent >= 0
-					&& settings.MaxInitialMissPercent >= 0;
-
-				if (reseed)
-				{
-					var prng = settings.Prng.Clone();
-					foreach (var kvp in layer.pendingExpansion)
-					{
-						kvp.Value.MissCount = prng.NextInt32(minMisses, maxMisses + 1);
-					}
-				}
-
-				return layer;
-			}
-
-			public Layer NextLayer(PRNG prng)
-			{
-				var prevLayer = this.shellItems;
-
-				int maxConsecutiveMisses = settings.MaxConsecutiveMisses;
-
-				List<(XZ, int)> expansion = new();
-
-				// Starting from a random point on the shell, do one lap.
-				// While one lap not completed:
-				// * choose random run length, subject to constraints
-				// * do or don't push out those shell items for next layer
-				// * flip the "do or don't push" flag for the next run
-				//
-				// To implement the "start from a random point on the shell" we
-				// * choose a random start index
-				// * loop from startIndex to startIndex + Count
-				// * use modulo during list indexing since our loopingIndex will (very likely) go past the end of the list
-				int startIndex = prng.NextInt32(prevLayer.Count);
-				int endIndex = startIndex + prevLayer.Count;
-				int loopingIndex = startIndex;
-				bool push = false;
-				do
-				{
-					push = !push;
-					if (push)
-					{
-
-						const int minRunLength = 2;
-						const int maxRunLength = 7;
-
-						int mustTakeNow = prevLayer.OneLapFrom(loopingIndex)
-							.TakeWhile(i => pendingExpansion[i.XZ].MissCount >= maxConsecutiveMisses)
-							.Take(maxRunLength)
-							.Count();
-
-						int runLength = prng.NextInt32(Math.Max(mustTakeNow, minRunLength), 1 + Math.Max(mustTakeNow, maxRunLength));
-						for (int i = 0; i < runLength; i++)
-						{
-							var prevItem = prevLayer[loopingIndex % prevLayer.Count];
-							loopingIndex++;
-							int elev = pendingExpansion[prevItem.XZ].Elevation;
-							expansion.Add((prevItem.XZ, elev));
-						}
-					}
-					else
-					{
-						const int minRunLength = 2;
-						//const int maxRunLength = 50;
-						int maxRunLength = Math.Min(prevLayer.Count / 2, 50);
-
-						int mustStopAt = prevLayer.OneLapFrom(loopingIndex)
-							.TakeWhile(i => pendingExpansion[i.XZ].MissCount < maxConsecutiveMisses)
-							.Take(maxRunLength)
-							.Count();
-
-						int runLength;
-						if (mustStopAt < minRunLength)
-						{
-							runLength = mustStopAt;
-						}
-						else
-						{
-							runLength = prng.NextInt32(minRunLength, 1 + Math.Min(mustStopAt, maxRunLength));
-						}
-						loopingIndex += runLength;
-					}
-				}
-				while (loopingIndex < endIndex);
-
-				foreach (var item in expansion)
-				{
-					pendingExpansion.Remove(item.Item1);
-				}
-				area.Expand(algorithm.ConvertExpansion(expansion));
-
-				return new Layer(this);
-			}
+			public int Elevation { get; init; }
 		}
-	}
 
-	class IntAlgorithm : Algorithm<int>
-	{
-		protected override IEnumerable<(XZ, int)> ConvertExpansion(IEnumerable<(XZ, int)> expansion) => expansion;
-	}
+		// Constants for run length calculations
+		// Should convert these to Settings and experiment...
+		private const int MinPushRunLength = 2;
+		private const int MaxPushRunLength = 7;
+		private const int MinSkipRunLength = 2;
+		private const int MaxSkipRunLengthDivisor = 2;
+		private const int MaxSkipRunLengthCap = 50;
 
-	class GenericAlgorithm<T> : Algorithm<T>
-	{
-		public required Func<int, T> Converter { get; init; }
+		private readonly ShellItemRing shellRing;
+		private readonly Dictionary<XZ, PendingShellItem> pendingExpansion;
+		private readonly Settings settings;
+		private readonly PRNG prng;
 
-		protected override IEnumerable<(XZ, T)> ConvertExpansion(IEnumerable<(XZ, int)> expansion)
+		public Layer(ShellItemRing shell, Dictionary<XZ, PendingShellItem> pendingExpansion, Settings settings)
 		{
-			return expansion.Select(a => (a.Item1, Converter(a.Item2)));
+			this.shellRing = shell;
+			this.pendingExpansion = pendingExpansion;
+			this.settings = settings;
+			this.prng = settings.Prng;
+		}
+
+		/// <summary>
+		/// Calculates the list of shell items to be pushed out for this layer.
+		/// </summary>
+		public List<(XZ XZ, int Elevation)> CalculateExpansion()
+		{
+			var expansion = new List<(XZ, int)>();
+			if (shellRing.Count == 0)
+			{
+				return expansion;
+			}
+
+			// Starting from a random point on the shell, do one lap, alternating between
+			// pushing and skipping runs of shell items.
+			int startIndex = prng.NextInt32(shellRing.Count);
+			int endIndex = startIndex + shellRing.Count;
+			int currentIndex = startIndex;
+			bool push = false;
+
+			do
+			{
+				push = !push;
+				int runLength;
+
+				if (push)
+				{
+					runLength = CalculatePushRunLength(currentIndex);
+					for (int i = 0; i < runLength; i++)
+					{
+						var item = shellRing[currentIndex % shellRing.Count];
+						currentIndex++;
+						int elev = pendingExpansion[item.XZ].Elevation;
+						expansion.Add((item.XZ, elev));
+					}
+				}
+				else
+				{
+					runLength = CalculateSkipRunLength(currentIndex);
+					currentIndex += runLength;
+				}
+			}
+			while (currentIndex < endIndex);
+
+			return expansion;
+		}
+
+		private int CalculatePushRunLength(int currentIndex)
+		{
+			int maxConsecutiveMisses = settings.MaxConsecutiveMisses;
+
+			// Determine how many items *must* be pushed because they have exceeded their miss count.
+			int mustTakeNow = shellRing.OneLapFrom(currentIndex)
+				.TakeWhile(i => pendingExpansion[i.XZ].MissCount >= maxConsecutiveMisses)
+				.Take(MaxPushRunLength)
+				.Count();
+
+			int minRun = Math.Max(mustTakeNow, MinPushRunLength);
+			int maxRun = Math.Max(mustTakeNow, MaxPushRunLength);
+
+			return prng.NextInt32(minRun, 1 + maxRun);
+		}
+
+		private int CalculateSkipRunLength(int currentIndex)
+		{
+			int maxConsecutiveMisses = settings.MaxConsecutiveMisses;
+			int maxRunLength = Math.Min(shellRing.Count / MaxSkipRunLengthDivisor, MaxSkipRunLengthCap);
+
+			// Determine how many items we can skip before hitting one that *must* be pushed.
+			int mustStopAt = shellRing.OneLapFrom(currentIndex)
+				.TakeWhile(i => pendingExpansion[i.XZ].MissCount < maxConsecutiveMisses)
+				.Take(maxRunLength)
+				.Count();
+
+			if (mustStopAt < MinSkipRunLength)
+			{
+				return mustStopAt;
+			}
+
+			return prng.NextInt32(MinSkipRunLength, 1 + Math.Min(mustStopAt, maxRunLength));
 		}
 	}
 }
